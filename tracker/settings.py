22f447730ce642ce3e3f4a28e4b70bdd971707e8
thomasfitz24 <thomasfitz2412@gmail.com>
2025-12-06 23:29:58 +0000

    Add custom login template and update settings

--- tracker/settings.py
+++ tracker/settings.py
@@ -103,3 +103,7 @@ STATIC_URL = "static/"
 
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGIN_URL = "/accounts/login/"
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/accounts/login/"
